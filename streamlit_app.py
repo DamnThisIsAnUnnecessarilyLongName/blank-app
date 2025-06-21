cd7d15796097c05500014f12054c96c56050dc0c
DamnThisIsAnUnnecessarilyLongName <53295576+DamnThisIsAnUnnecessarilyLongName@users.noreply.github.com>
2025-06-21 14:16:39 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,16 @@ import pandas as pd
 import polars as pl
 import fastexcel
 
+
+# Functions
+def create_metric(label, value, font_size="28px"):
+    return f"""
+    <div style='text-align: center; padding: 20px; background-color: #f8f9fa; border-radius: 10px;'>
+        <div style='font-size: 12px; color: #666;'>{label}</div>
+        <div style='font-size: {font_size}; font-weight: bold;'>{value}</div>
+    </div>
+    """
+#########################################################################
 st.title("🎈 What is this suburb like?")
 st.write(
     "Whether you are looking to move to a new suburb, or just curious to " \
@@ -75,8 +85,6 @@ df_stg = (
             .round(0).cast(pl.Int64) 
             .alias("avg_suburb_area_in_Council")
         )
-        .sort("Council", descending=False)
-        .filter(pl.col("Council") == "Darebin")
 
         .select("Council","Suburb","2024 Pop","avg_suburb_pop_in_Council",
                 "Suburb Area km^2","avg_suburb_area_in_Council",
@@ -84,21 +92,68 @@ df_stg = (
         
 )
 
+df_stg.columns = ["Council","Suburb","Population","avg_suburb_pop_in_Council",
+                "Area km^2","avg_suburb_area_in_Council",
+                "Population Change"]
+
+df_stg = df_stg.select("Council","Suburb","Population",
+                       "Area km^2","Population Change")
 # Create dropdown menu
 selected_category = st.selectbox(
     'Select Suburb:',
-    options=['All'] + list(df_stg['Suburb'].unique())
+    options=['All'] + list(df_stg['Suburb'].unique().sort())
 )
 
+# Summary
+title_col, select_col, hier_col = st.columns(3)
+
+with title_col:
+    st.metric(
+        label = "",
+        value = "# Residents"
+    )
+
 # Filter data based on selection
+
 if selected_category == 'All':
+    x = df_stg.select(pl.col('Population').sum()).item()
+    x1 = df_stg.select(pl.col('Population Change').sum()).item()
     filtered_df = df_stg
+
+    # Population Metric KPI
+    with select_col:
+        st.metric(
+            label = "VIC",
+            value = f"{x:,}",
+            delta = f"{x1:,}"
+        )
 else:
     filtered_df = (
         df_stg.filter(
             pl.col("Suburb") == selected_category
         )
     )
+
+    with select_col:
+        x = df_stg.filter(pl.col("Suburb") == selected_category).select(pl.col('Population').sum()).item()
+        x1 =df_stg.filter(pl.col("Suburb") == selected_category).select(pl.col('Population Change').sum()).item()
+        suburb = selected_category
+
+        st.metric(
+            label = suburb,
+            value = f"{x:,}",
+            delta = f"{x1:,}"
+        )
+    with hier_col:
+        sel_council = df_stg.filter(pl.col("Suburb") == selected_category).select(pl.col('Council')).item()
+        x = int(df_stg.filter(pl.col("Council") == sel_council).select(pl.col('Population').mean()).item())
+        x1 = int(df_stg.filter(pl.col("Council") == sel_council).select(pl.col('Population Change').mean()).item())
+        
+        st.metric(
+            label = sel_council + " Council Avg",
+            value = f"{x:,}",
+            delta = f"{x1:,}"
+        )
 
 # Display filtered data
 st.dataframe(
@@ -107,4 +162,9 @@ st.dataframe(
     height=400
     )
 
+
+
+
+""
 st.write("Source Data: Austraian Bureau of Statistics (ABS)")
+
